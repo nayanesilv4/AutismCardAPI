0cf05594b959484a8628a519ffd2b4bcbf48e20c
Nayane da Silva <nayanecosilva@gmail.com>
2025-04-25 09:36:15 -0300

    Delete autismcard directory

--- autismcard/src/main/java/com/autismcard/autismcard/repository/CardRepository.java
+++
@@ -1,7 +0,0 @@
-package com.autismcard.autismcard.repository;
-
-import com.autismcard.autismcard.model.Card;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CardRepository extends JpaRepository<Card, String> {
-}
